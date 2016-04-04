45dea29f2567a91ccdc8d4200c37302230b23e29
Andrei <pixelit@mail.ru>
2016-04-04 09:58:12 +0300

    Lesson 8 Наследование

--- Snake/VerticalLine.cs
+++ Snake/VerticalLine.cs
@@ -6,22 +6,14 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pList;
         public VerticalLine( int yTop, int yBottom, int x, char sym)
         {
             pList = new List<Point>();
             for (int y = yTop; y <= yBottom; y++){
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
-            }
-        }
-        public void Draw()
-        {
-            foreach(Point p in pList)
-            {
-                p.Draw();
             }
         }
     }
